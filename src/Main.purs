fa3e4386cc4960638301bbc7e9b8c80ac25f53d2
bouzuya <m@bouzuya.net>
2018-07-23 23:17:24 +0900

    add fetchRepos

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,20 @@
 module Main
   ( main ) where
 
+import Data.Maybe (Maybe)
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Class.Console (log)
 import Prelude (Unit)
+
+fetchRepos :: Aff (Maybe String)
+fetchRepos = do
+  response <- fetch
+    ( defaults
+    <> method := "GET"
+    <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
+    )
+  pure response.body
 
 main :: Effect Unit
 main = log "Hello"
