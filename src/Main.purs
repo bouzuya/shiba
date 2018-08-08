167119cd6440ccfc6822d605452dc0391a36a801
bouzuya <m@bouzuya.net>
2018-08-08 23:32:54 +0900

    show tags

--- src/Main.purs
+++ src/Main.purs
@@ -2,27 +2,39 @@ module Main
   ( main ) where
 
 import Bouzuya.DateTime (Time(..), exactDateFromWeekOfYear, weekOfYear, year)
-import Data.Array (intercalate)
+import Data.Array (intercalate, zip)
 import Data.Array as Array
 import Data.DateTime (DateTime(..))
 import Data.Maybe (fromJust, isJust, maybe)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
 import DateTimeFormat as DateTimeFormat
 import Effect (Effect)
-import Effect.Aff (error, launchAff_, throwError)
+import Effect.Aff (Aff, error, launchAff_, throwError)
 import Effect.Class (liftEffect)
-import Effect.Class.Console (log, logShow)
+import Effect.Class.Console (log)
 import Effect.Now (nowDate)
-import GitHub (fetchCommits, fetchRepos, fetchTags)
+import GitHub (Tag, Repo, fetchCommits, fetchRepos, fetchTags)
 import Partial.Unsafe (unsafePartial)
 import Prelude (Unit, bind, bottom, eq, map, pure, top, (&&), (<=), (<>))
 
+getTags :: DateTime -> DateTime -> Repo -> Aff (Array Tag)
+getTags fdt ldt repo = do
+  commitsMaybe <- fetchCommits repo fdt ldt
+  commits <- maybe (throwError (error "error")) pure commitsMaybe
+  tagsMaybe <- fetchTags repo
+  tags <- maybe (throwError (error "error")) pure tagsMaybe
+  pure
+    (Array.filter
+      (\tag -> isJust (Array.find (\commit -> eq tag.commit.sha commit.sha) commits))
+      tags
+    )
+
 main :: Effect Unit
 main = launchAff_ do
-  let user = "bouzuya"
-  reposMaybe <- fetchRepos user
-  repos <- maybe (throwError (error "error")) pure reposMaybe
   today <- liftEffect nowDate
   let
+    user = "bouzuya"
     y = year today
     woy = weekOfYear today
     fd = unsafePartial (fromJust (exactDateFromWeekOfYear y woy bottom))
@@ -31,18 +43,13 @@ main = launchAff_ do
     fdt = toDateTime fd
     ldt = toDateTime ld
     filter = Array.filter (\a -> fdt <= a.pushedAt && a.pushedAt <= ldt)
-    filtered = filter repos
-    repoMaybe = Array.head repos
-  repo <- maybe (throwError (error "error")) pure repoMaybe
-  commitsMaybe <- fetchCommits repo fdt ldt
-  commits <- maybe (throwError (error "error")) pure commitsMaybe
-  tagsMaybe <- fetchTags repo
-  tags <- maybe (throwError (error "error")) pure tagsMaybe
+  reposMaybe <- fetchRepos user
+  repos <- maybe (throwError (error "error")) pure reposMaybe
+  let filtered = filter repos
+  tags <- traverse (getTags fdt ldt) filtered
   let
-    filteredTags = Array.filter
-      (\tag -> isJust (Array.find (\commit -> eq tag.commit.sha commit.sha) commits))
-      tags
-  _ <- liftEffect (logShow filteredTags)
+    zipped :: Array (Tuple Repo (Array Tag))
+    zipped = zip filtered tags
   let
     dateLine =
       ( (DateTimeFormat.format DateTimeFormat.iso8601DateFormat fdt)
@@ -53,6 +60,6 @@ main = launchAff_ do
     (log
       (intercalate
         "\n"
-        ([dateLine, ""] <> (map (\s -> "- [" <> s <> "][]") (map _.fullName filtered)))
+        ([dateLine, ""] <> (map (\(Tuple r ts) -> "- [" <> r.fullName <> "][] …… " <> (intercalate "," (map _.name ts))) zipped))
       )
     )
