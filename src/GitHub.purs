b21fe26b022c657d63e24c8b457df6549f2549ca
bouzuya <m@bouzuya.net>
2018-08-08 15:48:59 +0900

    extract fetchRepos

---
+++ src/GitHub.purs
@@ -0,0 +1,64 @@
+module GitHub
+  ( Repo
+  , fetchRepos
+  ) where
+
+import Data.Argonaut (Json, jsonParser)
+import Data.Argonaut as Json
+import Data.Array as Array
+import Data.DateTime (DateTime)
+import Data.Either (Either, either)
+import Data.Maybe (Maybe(..))
+import Data.Options ((:=))
+import Data.Traversable (traverse)
+import DateTimeFormat as DateTimeFormat
+import Effect.Aff (Aff)
+import Fetch (fetch)
+import Fetch.Options (defaults, method, url)
+import Foreign.Object (Object)
+import Foreign.Object as Object
+import Prelude (bind, compose, const, join, map, pure, (<>))
+
+type Repo =
+  { fullName :: String
+  , pushedAt :: DateTime
+  }
+
+fetchRepos :: Aff (Maybe (Array Repo))
+fetchRepos = map (compose join (map parseRepos)) fetchRepos'
+
+fetchRepos' :: Aff (Maybe String)
+fetchRepos' = do
+  response <- fetch
+    ( defaults
+    <> method := "GET"
+    <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
+    )
+  pure response.body
+
+parseRepos :: String -> Maybe (Array Repo)
+parseRepos responseBody =
+  let
+    maybeFromEither :: forall a b. Either a b -> Maybe b
+    maybeFromEither = either (const Nothing) Just
+    toJson :: String -> Maybe Json
+    toJson = compose maybeFromEither jsonParser
+    toRecords :: Json -> Maybe (Array Repo)
+    toRecords json = do
+      array <- Json.toArray json
+      objects <- traverse Json.toObject array
+      pure (Array.catMaybes (map toRecord objects))
+    toRecord :: Object Json -> Maybe Repo
+    toRecord o = do
+      fullName <- bind (Object.lookup "full_name" o) Json.toString
+      pushedAtString <- bind (Object.lookup "pushed_at" o) Json.toString
+      pushedAt <-
+        either
+          (const Nothing)
+          Just
+          (DateTimeFormat.parse
+            DateTimeFormat.iso8601DateTimeFormatWithoutMilliseconds
+            pushedAtString)
+      pure { fullName, pushedAt }
+  in
+    bind (toJson responseBody) toRecords
--- src/Main.purs
+++ src/Main.purs
@@ -21,13 +21,9 @@ import Fetch (fetch)
 import Fetch.Options (defaults, method, url)
 import Foreign.Object (Object)
 import Foreign.Object as Object
+import GitHub (Repo, fetchRepos)
 import Partial.Unsafe (unsafePartial)
 import Prelude (Unit, bind, bottom, compose, const, join, map, pure, top, (&&), (<=), (<>))
-
-type Repo =
-  { fullName :: String
-  , pushedAt :: DateTime
-  }
 
 fetchCommits :: String -> DateTime -> DateTime -> Aff (Maybe String)
 fetchCommits fullName since until = do
@@ -50,45 +46,9 @@ fetchTags { fullName } = do
     )
   pure response.body
 
-fetchRepos :: Aff (Maybe String)
-fetchRepos = do
-  response <- fetch
-    ( defaults
-    <> method := "GET"
-    <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
-    )
-  pure response.body
-
-parseRepos :: String -> Maybe (Array Repo)
-parseRepos responseBody =
-  let
-    maybeFromEither :: forall a b. Either a b -> Maybe b
-    maybeFromEither = either (const Nothing) Just
-    toJson :: String -> Maybe Json
-    toJson = compose maybeFromEither jsonParser
-    toRecords :: Json -> Maybe (Array Repo)
-    toRecords json = do
-      array <- Json.toArray json
-      objects <- traverse Json.toObject array
-      pure (Array.catMaybes (map toRecord objects))
-    toRecord :: Object Json -> Maybe Repo
-    toRecord o = do
-      fullName <- bind (Object.lookup "full_name" o) Json.toString
-      pushedAtString <- bind (Object.lookup "pushed_at" o) Json.toString
-      pushedAt <-
-        either
-          (const Nothing)
-          Just
-          (DateTimeFormat.parse
-            DateTimeFormat.iso8601DateTimeFormatWithoutMilliseconds
-            pushedAtString)
-      pure { fullName, pushedAt }
-  in
-    bind (toJson responseBody) toRecords
-
 main :: Effect Unit
 main = launchAff_ do
-  reposMaybe <- map (compose join (map parseRepos)) fetchRepos
+  reposMaybe <- fetchRepos
   repos <- maybe (throwError (error "error")) pure reposMaybe
   today <- liftEffect nowDate
   let
